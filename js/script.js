d1ed02789592fa19e24b04bea09b946aceb5f4cf
Rutusy <92893127+Rutusy@users.noreply.github.com>
2022-02-03 14:33:13 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,10 +4,15 @@ function closePopup () {
 	console.log(openedPopup.length);
 	for (let h=0; h < openedPopup.length; h++) {
 		if (openedPopup[h].style.display = 'block') {
-			openedPopup[h].addEventListener("click", function(){
-				console.log('Отрктый попап под номером'+h);
-			};)
-		};
+			openedPopup[h].addEventListener("click", gogo);
+			function gogo (e) {
+				console.log(this);
+				console.log(e.target);
+			}
+		}
+		if (openedPopup[h].style.display = 'none') {
+			console.log('Скрытый '+h);
+		} 
 	};
 };
 
